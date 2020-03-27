48be1c43de5208462d26a3246443b799da3cf90d
Da1anna <1318176575@qq.com>
2020-03-27 19:26:43 +0800

    2020/3/27

--- leetcode/图/图的基础.py
+++ leetcode/图/图的基础.py
@@ -42,7 +42,7 @@ class Graph:
     def __contains__(self, key):
         return key in self.vertList
 
-    def addEdge(self,f,t,cost):
+    def addEdge(self,f,t,cost=0):
         if f not in self.vertList:
             self.addVertex(f)
         if t not in self.vertList:
@@ -55,21 +55,135 @@ class Graph:
     def __iter__(self):
         return iter(self.vertList.values())
 
-g = Graph()
-for i in range(6):
-    g.addVertex(i)
-print(g.vertList)
-print(g.getVertices())
 
-g.addEdge(0,1,5)
-g.addEdge(0,5,2)
-g.addEdge(1,2,4)
-g.addEdge(2,3,9)
-g.addEdge(3,4,7)
-g.addEdge(3,5,3)
-g.addEdge(4,0,1)
-g.addEdge(5,4,8)
-g.addEdge(5,2,1)
-for v in g:
-    for w in v.getConnections():
-        print("( %s--%s : %s )" % (v.getId(), w.getId(), v.getWeight(w)))
+# g = Graph()
+# for i in range(6):
+#     g.addVertex(i)
+# print(g.vertList)
+# print(g.getVertices())
+#
+# g.addEdge(0,1,5)
+# g.addEdge(0,5,2)
+# g.addEdge(1,2,4)
+# g.addEdge(2,3,9)
+# g.addEdge(3,4,7)
+# g.addEdge(3,5,3)
+# g.addEdge(4,0,1)
+# g.addEdge(5,4,8)
+# g.addEdge(5,2,1)
+# for v in g:
+#     for w in v.getConnections():
+#         print("( %s--%s : %s )" % (v.getId(), w.getId(), v.getWeight(w)))
+
+
+'''
+2.广度优先遍历BFS和深度优先遍历DFS
+注:这里考虑的是强连通图
+'''
+
+#初始化图
+# vertex_1= [1,2,3,4,5,6]
+# edge_1 = [(1,2),(1,6),(2,3),(2,5),(3,4),(3,1),(4,1),(4,5),(5,3),(5,4)]
+#
+# vertex_2= [1,2,3,4,5]
+# edge_2 = [(1,2),(1,3),(2,4),(2,5)]
+# g = Graph()
+# for v in vertex_2:
+#     g.addVertex(v)
+# for cur,edge in edge_2:
+#     g.addEdge(cur,edge)
+# for v in g:
+#     for w in v.getConnections():
+#          print("( %s--%s )" % (v.getId(), w.getId()))
+
+#2.1 BFS
+def BFS(g:Graph,start:Vertex):
+    queue = []
+    visited = set()
+    queue.append(start)
+    visited.add(start)
+
+    while queue:
+        cur = queue.pop(0)
+        for nbr in cur.getConnections():
+            if nbr not in visited:
+                queue.append(nbr)
+                visited.add(nbr)
+        print(cur.id)
+
+# BFS(g,g.getVertex(1))
+
+#2.2 DFS
+def DFS(g:Graph,start:Vertex):
+    stack = []
+    stack.append(start)
+    visited = set()
+    visited.add(start)
+
+    res = [start.id]
+    while stack:
+        cur = stack[-1]
+        found_newV = False      #记录是否找到cur的未访问过的邻节点
+        # res.append(cur)
+        for nbr in cur.getConnections():
+            if nbr not in visited:
+                #发现一个未被访问的节点的处理：
+                visited.add(nbr)
+                stack.append(nbr)
+                res.append(nbr.id)
+                found_newV = True
+                break
+            else:
+                continue
+        if  not found_newV:
+            stack.pop()
+
+    print(res)
+
+# DFS(g,g.getVertex(1))
+
+
+'''
+3 最短路径——Dijkstra算法
+'''
+from pythonds import PriorityQueue,Graph,Vertex
+
+def dijkstra(aGraph:Graph,start:Vertex):
+    pq = PriorityQueue()
+    start.setDistance(0)
+    pq.buildHeap([(v.getDistance(),v) for v in aGraph])
+
+    while not pq.isEmpty():
+        curV = pq.delMin()
+        for nextV in curV.getConnections():
+            newDist = curV.getDistance() + curV.getWeight(nextV)
+            if newDist < nextV.getDistance():
+                nextV.setDistance(newDist)
+                nextV.setPred(curV)
+                pq.decreaseKey(nextV,newDist)   #这个方法应该是内部调用了percUp(),有调整堆的味道
+
+#初始化图
+v = ['u','v','x','w','y','z']
+#无向图一定要这样创建吗？
+edge = [('u','v',2),('u','w',5),('u','x',1),
+        ('v','u',2),('v','x',2),('v','w',3),
+        ('x','u',1),('x','v',2),('x','w',3),('x','y',1),
+        ('w','v',2),('w','u',5),('w','x',3),('w','y',1),('w','z',5),
+        ('y','x',1),('y','w',1),('y','z',1),
+        ('z','w',5),('z','y',1)]
+
+g = Graph()
+g.addVertex(v for v in v)
+for cur,nbr,weight in edge:
+    g.addEdge(cur, nbr, weight)
+# print(g.getVertex('w'))
+
+#测试
+dijkstra(g,g.getVertex('u'))
+for v in v[1:]:
+    print('u到%s的最短距离：%i' % (v,g.getVertex(v).getDistance()))
+
+
+'''
+4.最小生成树
+'''
--- leetcode/树与二叉树/demo.py
+++ leetcode/树与二叉树/demo.py
@@ -3,6 +3,16 @@
 # lst = [1,2,3]
 # print(lst[1:2])
 
-dict = {'a':1,'b':2}
-for x in dict:
-    print(x)
+# dict = {'a':1,'b':2}
+# # for x in dict:
+#     # print(x)
+#
+# lst = [1,
+#        2,
+#        3,
+#        4,
+#        5]
+# demo = [1,2]
+#
+# bol = [1,2,3] in lst
+# print(bol)
