3d274893666c86ef93a5a2ddf5b1b72b84b1601b
Da1anna <1318176575@qq.com>
2020-04-03 19:23:51 +0800

    2020/4/1

--- leetcode/树与二叉树/common.py
+++ leetcode/树与二叉树/common.py
@@ -77,7 +77,7 @@ root = list_buildTree_2(lst)
 链接：https://leetcode-cn.com/problems/path-sum-ii
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
-def f(root:TreeNode,sum_:int):
+def pathSum2(root:TreeNode,sum_:int):
     res = []
     stack = [([root.key],root)]
 
@@ -94,7 +94,7 @@ def f(root:TreeNode,sum_:int):
 
 #测试
 
-# res = f(root,22)
+# res = pathSum2(root,22)
 # print(res)
 
 
@@ -121,7 +121,8 @@ def f(root:TreeNode,sum_:int):
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
 
-def pathSum1(root:TreeNode,sum_) -> bool:
+#非递归解法：用栈实现先序遍历
+def pathSum1_NR(root:TreeNode,sum_) -> bool:
     stack = [(root.key,root)]
     found = False
 
@@ -137,8 +138,114 @@ def pathSum1(root:TreeNode,sum_) -> bool:
     return found
 
 #测试
-res = pathSum1(root,22)
-print(res)
+# res = pathSum_NR(root,22)
+# print(res)
+
+
+'''
+3.路径总和III
+给定一个二叉树，它的每个结点都存放着一个整数值。
+
+找出路径和等于给定数值的路径总数。
+
+路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+
+示例：
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+返回 3。和等于 8 的路径有:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3.  -3 -> 11
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+#双重递归
+def pathSum3(root:TreeNode,sum_) -> int:
+    #整棵树的每个节点都dfs一遍
+    if not root:
+        return 0
+    return dfs(root,sum_) + pathSum3(root.left,sum_) + pathSum3(root.right,sum_)
+
+def dfs(root,sum_):
+    #从某一顶点开始的先序遍历
+    if not root:
+        return 0
+    tmp = sum_ - root.key
+    return (1 if tmp == 0 else 0) + dfs(root.left,tmp) + dfs(root.right,tmp)
+
+#测试
+# lst=[10,5,-3,3,2,None,11,3,-2,None,1]
+# sum = 8
+# root = list_buildTree_2(lst)
+#
+# res = pathSum3(root,sum)
+# print(res)
+
+'''
+4.最长同值路径
+给定一个二叉树，找到最长的路径，这个路径中的每个节点具有相同值。 这条路径可以经过也可以不经过根节点。
+
+注意：两个节点之间的路径长度由它们之间的边数表示。
+
+示例 1:
+
+输入:
+
+              5
+             / \
+            4   5
+           / \   \
+          1   1   5
+输出:
+
+2
+示例 2:
+
+输入:
+
+              1
+             / \
+            4   5
+           / \   \
+          4   4   5
+输出:
+
+2
+注意: 给定的二叉树不超过10000个结点。 树的高度不超过1000。
+'''
+
+# def LongestPath(root:TreeNode) -> int:
+
+
+#测试
+# LongestPath(root)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
